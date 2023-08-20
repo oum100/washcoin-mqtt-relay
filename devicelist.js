1ac17374bc8e25a42eb6a27edcbeddf6c02e9c7f
oum100 <L.Teerin@gmail.com>
2023-08-21 01:40:48 +0700

    remark Df-96

--- devicelist.js
+++ devicelist.js
@@ -1,33 +1,33 @@
 const deviceList = [
-    {
-        "source":{
-            "name":"DF-96",
-            "uuid":"GH485S9W5482OQ2W5X7Z",
-            "type":"dryer",            
-            "subscribeTopic":{
-                "prefix":"washcoin/",
-                "subfix":"",
-            },
-            "publishTopic":{
-                "prefix":"washcoin/",
-                "subfix":"",
-            },
+    // {
+    //     "source":{
+    //         "name":"DF-96",
+    //         "uuid":"GH485S9W5482OQ2W5X7Z",
+    //         "type":"dryer",            
+    //         "subscribeTopic":{
+    //             "prefix":"washcoin/",
+    //             "subfix":"",
+    //         },
+    //         "publishTopic":{
+    //             "prefix":"washcoin/",
+    //             "subfix":"",
+    //         },
             
-        },
-        "target":{
-            "name":"DF-96",
-            "uuid":"C049EFEFD5CC",
-            "type":"dryer",
-            "subscribeTopic":{
-                "prefix":"rgh18/stat/",
-                "subfix":"",
-            },
-            "publishTopic":{
-                "prefix":"rgh18/cmnd/",
-                "subfix":"/json",
-            },   
-        }
-    },
+    //     },
+    //     "target":{
+    //         "name":"DF-96",
+    //         "uuid":"C049EFEFD5CC",
+    //         "type":"dryer",
+    //         "subscribeTopic":{
+    //             "prefix":"rgh18/stat/",
+    //             "subfix":"",
+    //         },
+    //         "publishTopic":{
+    //             "prefix":"rgh18/cmnd/",
+    //             "subfix":"/json",
+    //         },   
+    //     }
+    // },
     {
         "source":{
             "name":"WF-103",
